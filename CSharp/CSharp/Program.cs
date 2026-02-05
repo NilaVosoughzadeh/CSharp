5d9c939bb6f7c645599eaa6133f64a18a39534e2
Nila <nilavosoughzadeh@gmail.com>
2026-02-05 11:23:27 +0330

    5.2.2025

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -10,10 +10,27 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            string Name = "Nila";
+            int a = 4, b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.WriteLine(Name);
+
+            //Convert
+            int d = 20;
+            string number = Convert.ToString(d);
+            Console.WriteLine(number);
+            Console.ReadKey();
+
+            //Exeption
+            int num1, num2;
+            Console.WriteLine("Please enter number 1 :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter number 2 :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("Sum is : " + sum);
+            Console.ReadKey();
         }
-    }
-    class Student
-    {
-        string Name = "Nila";
     }
 }
